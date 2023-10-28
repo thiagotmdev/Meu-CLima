7d1659c5c20fafb188afaef3876b9335cccd232e
Thiago Mendes <thiagomendes.dev@gmail.com>
2023-10-28 19:41:25 -0300

    tag head, meta e title adicionadas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import { Clima } from '../../components/Clima';
 
@@ -8,6 +7,13 @@ import { Clima } from '../../components/Clima';
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Meu Clima</title>
+      <meta name="author" content="Thiago Mendes - Arckmake Studio"/>
+      <meta name="keywords" content="HTML, CSS, JavaScript, NextJS, ReactJS, Axios"/>
+      <meta name="description" content="Aplicação feita em NextJS, que mostra os dados do clima no dia na cidade pesquisada" />
+      <meta charSet="UTF-8"/>
+    </Head>
       <div className={styles.container}>
         <header>
           <h1 className={styles.title}>Meu Clima</h1>
